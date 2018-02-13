065427f00af64cadeca33c0700e1fe729b8f47f8
T. Bruno Moretti <moretty@gmail.com>
2018-02-13 10:11:40 -0200

    Add !mute and !unmute commands

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ client = discord.Client()
 ADMIN_ROLE_ID = '400770147922739200'
 MODERATOR_ROLE_ID = '401477585038606347'
 TWITCH_ROLE_ID = '402588361988243466'
+MUTED_ROLE = '412942382292664332'
 ALLOWED_ROLES = [
     { 'role': 'Grand Master', 'term': ['grand master', 'master'] },
     { 'role': 'Diamond', 'term': ['diamond', 'diamante'] },
@@ -40,52 +41,59 @@ async def on_message(message):
     author_roles = [a.id for a in message.author.roles]
 
     if ADMIN_ROLE_ID in author_roles or MODERATOR_ROLE_ID in author_roles:
-        return
+        if content.startswith('!mute') or content.startswith('!unmute'):
+            role = discord.utils.get(message.server.roles, id=MUTED_ROLE)
+            user = message.mentions[0]
 
-    if message.attachments and message.channel.name == 'geral':
-        msg = '{0.author.mention}, não é permitido enviar anexos (imagens, arquivos, etc) aqui!'.format(message)
+            if content.startswith('!mute'):
+                await client.add_roles(user, role)
+            else:
+                await client.remove_roles(user, role)
+    else:
+        if message.attachments and message.channel.name == 'geral':
+            msg = '{0.author.mention}, não é permitido enviar anexos (imagens, arquivos, etc) aqui!'.format(message)
 
-        await client.send_message(message.channel, msg)
-        await client.delete_message(message)
-
-    pattern = 'https?:\/\/(?:www\.)?twitch\.tv\/(\w+)'
-    twitch_match = re.search(pattern, content)
-
-    if twitch_match:
-        if TWITCH_ROLE_ID not in author_roles:
+            await client.send_message(message.channel, msg)
             await client.delete_message(message)
 
-    pattern = 'https?:\/\/discord\.gg\/(\w+)'
-    discord_match = re.search(pattern, content)
+        pattern = 'https?:\/\/(?:www\.)?twitch\.tv\/(\w+)'
+        twitch_match = re.search(pattern, content)
 
-    if discord_match:
-        code = discord_match.group(1)
-        r = requests.get("https://discordapp.com/api/v6/invites/%s" % code)
-        data = json.loads(r.text)
+        if twitch_match:
+            if TWITCH_ROLE_ID not in author_roles:
+                await client.delete_message(message)
 
-        if data.get('guild').get('id') != os.environ.get('DISCORD_MAIN_SERVER_ID'):
-            await client.delete_message(message)
+        pattern = 'https?:\/\/discord\.gg\/(\w+)'
+        discord_match = re.search(pattern, content)
 
-    if content.startswith('!elo'):
-        entered_rank = content[5:].lower().strip()
-        role_name = [item['role'] for item in ALLOWED_ROLES if entered_rank in item['term']]
+        if discord_match:
+            code = discord_match.group(1)
+            r = requests.get("https://discordapp.com/api/v6/invites/%s" % code)
+            data = json.loads(r.text)
 
-        if role_name:
-            role = discord.utils.get(message.server.roles, name=role_name[0])
-            valid_roles = [i['role'] for i in ALLOWED_ROLES]
+            if data.get('guild').get('id') != os.environ.get('DISCORD_MAIN_SERVER_ID'):
+                await client.delete_message(message)
 
-            user_roles = message.author.roles
-            user_roles = [i for i in user_roles if i.name not in valid_roles]
+        if content.startswith('!elo'):
+            entered_rank = content[5:].lower().strip()
+            role_name = [item['role'] for item in ALLOWED_ROLES if entered_rank in item['term']]
 
-            user_roles.append(role)
+            if role_name:
+                role = discord.utils.get(message.server.roles, name=role_name[0])
+                valid_roles = [i['role'] for i in ALLOWED_ROLES]
 
-            msg = '{0.author.mention}, mudei seu elo para {1}!'.format(message, role.name)
+                user_roles = message.author.roles
+                user_roles = [i for i in user_roles if i.name not in valid_roles]
 
-            await client.replace_roles(message.author, *user_roles)
-            await client.send_message(message.channel, msg)
-        else:
-            msg = '{0.author.mention}, os elos possíveis são: Grand Master, Diamond, Platinum, Gold, Silver ou Bronze.'.format(message)
-            await client.send_message(message.channel, msg)
+                user_roles.append(role)
+
+                msg = '{0.author.mention}, mudei seu elo para {1}!'.format(message, role.name)
+
+                await client.replace_roles(message.author, *user_roles)
+                await client.send_message(message.channel, msg)
+            else:
+                msg = '{0.author.mention}, os elos possíveis são: Grand Master, Diamond, Platinum, Gold, Silver ou Bronze.'.format(message)
+                await client.send_message(message.channel, msg)
 
 
 client.run(os.environ.get('DISCORD_TOKEN'))
